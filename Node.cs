e02b852690f6ea2ca9b048bd97f19779c15a3f2a
TimH <tmherron09@gmail.com>
2020-07-31 13:33:51 -0500

    Init Commit

---
+++ Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeClass
+{
+    public class Node
+    {
+        public int data;
+        public Node leftNode;
+        public Node rightNode;
+
+
+    }
+}
